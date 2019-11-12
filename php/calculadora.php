64c3b45632e968d469eb2615b67e33b6c6972bf7
Amauri-Costa <amauricosta2002@gmial.com>
2019-11-11 23:32:24 -0200

    ATUALIZAÇÃO AULA 06 DIA 11.11.2019

---
+++ php/calculadora.php
@@ -0,0 +1,74 @@
+<?php 
+
+//sistema de calculadora
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+$_ehPost = $method == "POST";
+
+if ($_ehPost) {
+	
+$n1    = $_POST["n1"];
+$n2    = $_POST["n2"];
+
+$soma = $n1 + $n2;
+$sub  = $n1 - $n2;
+$mult = $n1 * $n2;
+$div  = $n1 / $n2;
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Calculadora</title>
+	<link rel="stylesheet" type="text/css" href="../css/estilo.css">
+</head>
+<body>
+
+<hr>
+	<form method="POST" action="calculadora.php">
+		<fieldset>
+		<legend>Calculadora</legend>
+
+		<label>Número 1: </label>
+		  <input type="number" name="n1" placeholder="Digite um número" autofocus required><br><br>
+
+        <label>Número 2: </label>
+		  <input type="number" name="n2" placeholder="Digite um número" autofocus required><br><br>
+
+		 <input type="submit" name="" value="Enter">
+		 <input type="reset" name="" value="Limpar"><br><br>
+        </fieldset>
+
+	</form>
+
+
+<hr>	
+		
+<h1>Calculadora</h1>
+
+<h2>Resultados</h2>
+<?php 
+
+	if ($_ehPost) {
+		
+	
+	print "Número 1 = $n1        <br>";
+	print "Número 2 = $n2  		 <br>";
+	print "<hr>"; 
+	//
+	print "a soma 		  = $soma  <br>";
+	print "Subtração	  = $sub   <br>";
+	print "Multiplicação  = $mult  <br>";
+	print "Divisão        = $div   <br>";
+	//
+	
+	}
+?>
+
+</body>
+</html>
